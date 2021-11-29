319fbc587fded14966a7f82e645d476465a2ad83
Ali Naseri <mo.ali.naseri@gmail.com>
2021-11-29 17:03:57 +0330

    fix update cart after adding product

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,13 +1,21 @@
-import { useSelector } from "react-redux";
+import { useLayoutEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import PageTitle from "../../components/PageTitle"
 import CartProductSkeleton from "../../components/Products/CartProduct/CartProductSkeleton";
 import { MainLayout } from "../../layouts"
+import { retrieveCart } from "../../store/reducer/cartSlice";
 import "./cart.css";
 import CartInfo from "./CartInfo";
 import CartItems from "./CartItems";
 
 function Cart() {
-  const {items, info: cart, loading} = useSelector(({cart}) => cart);
+  const {items, info: cart, loading, id: cartId} = useSelector(({cart}) => cart);
+  const dispatch = useDispatch();
+
+  useLayoutEffect(() => {
+    if(cartId == null) return;
+    dispatch(retrieveCart(cartId));
+  }, [cartId, dispatch]);
 
   return (
     <MainLayout>
